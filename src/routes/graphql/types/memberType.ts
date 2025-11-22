9aa480c7ed10398c1f1f195db4901628be20355b
iliaShamakhia <shamakhiailia@gmail.com>
2025-11-22 15:31:43 +0400

    feat: add resolvers fix for failing tests

--- src/routes/graphql/types/memberType.ts
+++ src/routes/graphql/types/memberType.ts
@@ -1,10 +1,17 @@
-import { GraphQLFloat, GraphQLInt, GraphQLObjectType, GraphQLString } from 'graphql';
-import { UUIDType } from './uuid.js';
+import { GraphQLEnumType, GraphQLFloat, GraphQLInt, GraphQLObjectType } from 'graphql';
+
+export const MemberTypeIdEnum = new GraphQLEnumType({
+  name: 'MemberTypeId',
+  values: {
+    BASIC: { value: 'BASIC' },
+    BUSINESS: { value: 'BUSINESS' },
+  },
+});
 
 export const MemberTypeType = new GraphQLObjectType({
   name: 'MemberType',
   fields: () => ({
-    id: { type: UUIDType },
+    id: { type: MemberTypeIdEnum },
     discount: { type: GraphQLFloat },
     postsLimitPerMonth: { type: GraphQLInt },
   }),
--- src/routes/graphql/types/userType.ts
+++ src/routes/graphql/types/userType.ts
@@ -1,5 +1,7 @@
-import { GraphQLObjectType, GraphQLString } from 'graphql';
+import { GraphQLList, GraphQLObjectType, GraphQLString } from 'graphql';
 import { UUIDType } from './uuid.js';
+import { ProfileType } from './profileType.js';
+import { PostType } from './postType.js';
 
 export const UserType = new GraphQLObjectType({
   name: 'UserType',
@@ -7,5 +9,9 @@ export const UserType = new GraphQLObjectType({
     id: { type: UUIDType },
     name: { type: GraphQLString },
     balance: { type: GraphQLString },
+    profile: { type: ProfileType },
+    posts: { type: new GraphQLList(PostType) },
+    userSubscribedTo: { type: new GraphQLList(UserType) },
+    subscribedToUser: { type: new GraphQLList(UserType) }
   }),
 });
