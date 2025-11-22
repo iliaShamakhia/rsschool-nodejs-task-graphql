2ade9e2a724e0ff3472f59bceb66ccf5b328a304
iliaShamakhia <shamakhiailia@gmail.com>
2025-11-22 14:55:01 +0400

    init: add all resources implementation

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,9 +1,25 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, GraphQLList, GraphQLObjectType, GraphQLSchema } from 'graphql';
+import { MemberTypeType } from './types/memberType.js';
+import { PostType } from './types/postType.js';
+import { UserType } from './types/userType.js';
+import { ProfileType } from './types/profileType.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
+
+  const getAllResourcesSchema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+      name: 'getAllResourcesSchema',
+      fields: {
+        memberTypes: { type: new GraphQLList(MemberTypeType), resolve: () => prisma.memberType.findMany() },
+        posts: { type: new GraphQLList(PostType), resolve: () => prisma.post.findMany() },
+        users: { type: new GraphQLList(UserType), resolve: () => prisma.user.findMany() },
+        profiles: { type: new GraphQLList(ProfileType), resolve: () => prisma.profile.findMany() },
+      },
+    }),
+  });
 
   fastify.route({
     url: '/',
@@ -15,7 +31,13 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      // return graphql();
+      const { query, variables } = req.body;
+      const result = await graphql({
+        schema: getAllResourcesSchema,
+        source: query,
+        variableValues: variables,
+      });
+      return result;
     },
   });
 };
---
+++ src/routes/graphql/types/userType.ts
@@ -0,0 +1,11 @@
+import { GraphQLObjectType, GraphQLString } from 'graphql';
+import { UUIDType } from './uuid.js';
+
+export const UserType = new GraphQLObjectType({
+  name: 'UserType',
+  fields: () => ({
+    id: { type: UUIDType },
+    name: { type: GraphQLString },
+    balance: { type: GraphQLString },
+  }),
+});
